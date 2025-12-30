932642791cd6948db1900b10c1853901d4febe30
Vrinda <vrindaunni13@gmail.com>
2025-12-30 12:18:53 +0530

    changed

--- service_api/urls.py
+++ service_api/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for Job_api project.
+URL configuration for service_api project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/6.0/topics/http/urls/
--- service_api/wsgi.py
+++ service_api/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for Job_api project.
+WSGI config for service_api project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Job_api.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'service_api.settings')
 
 application = get_wsgi_application()
